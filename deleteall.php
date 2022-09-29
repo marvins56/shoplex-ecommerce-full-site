a67dc5dd389f699ef3afbc0870b0302012a25499
marvins56 <82571414+marvins56@users.noreply.github.com>
2022-09-29 13:50:29 +0300

    paw

--- deleteall.php
+++ deleteall.php
@@ -1,23 +1,44 @@
-<?php
-include('database.php');
-session_start();
-
-$username = $_SESSION['username'];
-$query = "DELETE from cart where username = '$username' ";
-$result = mysqli_query($conn,$query);
-if($result){
-  if(isset($_SESSION['url'])){
-  $backlink =  $_SESSION['url'];
-  header("location:$backlink");
-}else{
-  header("category.php");
-}
-}
- else{
-   die(mysqli_error($conn));
- }
-
-
-
-
- ?>
+<?php
+
+include('database.php');
+
+session_start();
+
+$username = $_SESSION['username'];
+
+$query = "DELETE from cart where username = '$username' ";
+
+$result = mysqli_query($conn,$query);
+
+if($result){
+
+  if(isset($_SESSION['url'])){
+
+  $backlink =  $_SESSION['url'];
+
+  header("location:$backlink");
+
+}else{
+
+  header("category.php");
+
+}
+
+}
+
+ else{
+
+   die(mysqli_error($conn));
+
+ }
+
+
+
+
+
+
+
+
+
+ ?>
+
